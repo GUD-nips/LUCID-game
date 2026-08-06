272dbd497447f1c5af732fae125381b5019b7326
Amadeus <ezra.barker.7@gmail.com>
2026-08-06 12:10:42 -0700

    First commit for LUCID game project.

---
+++ 05 Scripts/MainMenuScript.gd
@@ -0,0 +1,21 @@
+extends MarginContainer
+
+
+func _on_exit_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_continue_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_load_menu_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_new_game_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
